419a9a61aca168aa7576950f9a4e2b3f22f77679
Mehmet Turan <mattyturan@gmail.com>
2026-07-11 10:10:56 -0700

    Debugging related console logs added to events.ts (Railway Server Response Debugging). WiP-2 AND Added SSL check.

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -7,6 +7,9 @@ dotenv.config()
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false
 })
 
 export const db = drizzle(pool, { schema })
