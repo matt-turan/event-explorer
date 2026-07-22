fd803f67910587fed260f44472b6d01f5b7f9775
Mehmet Turan <mattyturan@gmail.com>
2026-07-22 03:46:56 -0700

    Created a store using Zustand package. Included persist store.

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,46 +1,57 @@
 import { useState } from 'react'
 import { searchEvents, saveEvent } from '../services/api'
-import type { Event, EventSearchResponse } from '../types/event'
-// import { useSearchStore } from '../stores/searchStore'
+// import type { Event, EventSearchResponse } from '../types/event'
+import type { Event } from '../types/event'
+import { useSearchStore } from '../stores/searchStore'
 
 export default function SearchPage() {
-    const [keyword, setKeyword] = useState('')
-    const [city, setCity] = useState('')
-    const [page, setPage] = useState(1)
-    const [results, setResults] = useState<EventSearchResponse | null>(null)
-    const [savedIds, setSavedIds] = useState<Set<string>>(new Set())
+    // const [keyword, setKeyword] = useState('')
+    // const [city, setCity] = useState('')
+    // const [page, setPage] = useState(1)
+    // const [results, setResults] = useState<EventSearchResponse | null>(null)
+    // const [savedIds, setSavedIds] = useState<Set<string>>(new Set())
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState<string | null>(null)
-    
 
-    // const {
-    //     keyword, city, page, results, savedIds,
-    //     setKeyword, setCity, setPage, setResults, addSavedId
-    // } = useSearchStore()
+    const {
+        keyword, city, page, results, savedIds,
+        setKeyword, setCity, setPage, setResults, addSavedId
+    } = useSearchStore()
+
+    console.log('useSearchStore ', useSearchStore)
+    console.log('keyword ', keyword)
+    console.log('city ', city)
+    console.log('page ', page)
+    console.log('results ', results)
+    console.log('savedIds ', savedIds)
+
+
 
 
     // Calculate which page numbers to show in the window
     // Outside return, after your state declarations
-    const windowSize = 10
+    const windowSize = 6
     const currentWindow = Math.ceil(page / windowSize)
-    console.log('page ', page)
-    console.log('currentWindow ', currentWindow)
+    // console.log('page ', page)
+    // console.log('currentWindow ', currentWindow)
     const windowStart = (currentWindow - 1) * windowSize + 1
-    console.log('windowStart ', windowStart)
+    // console.log('windowStart ', windowStart)
     const windowEnd = results
     ? Math.min(currentWindow * windowSize, results.totalPages)
     : windowSize
-    console.log('windowEnd ', windowEnd)
+    // console.log('windowEnd ', windowEnd)
 
     const handleSearch = async (e: React.SubmitEvent<HTMLFormElement>) => {
         e.preventDefault()
+
         if (!keyword && !city) return
+
         setLoading(true)
         setError(null)
         setPage(1)
+
         try {
             const data = await searchEvents(keyword, city, 1)
-            // const data = await searchEvents(keyword, city, 1)
             console.log('Search results:', data) // Log the search results for debugging
             setResults(data)
         } catch {
@@ -52,12 +63,11 @@ export default function SearchPage() {
 
     const handlePageChange = async (keyword: string, city: string, newPage: number) => {
         console.log(`Changing to page: ${newPage} --- and current page: ${page}`) // Log the new page number for debugging
-        
+
         if (newPage < 1 || (results && newPage > results.totalPages) ) return
-        // if (newPage > totalPages) {
-        //     setTotalPages(totalPages + 10) // Update totalPages if newPage exceeds it
-        // }
+
         setPage(newPage)
+
         try {
             const data = await searchEvents(keyword, city, newPage)
             console.log('Search results:', data) // Log the search results for debugging
@@ -70,11 +80,8 @@ export default function SearchPage() {
     const handleSave = async (event: Event) => {
         try {
             await saveEvent(event)
-            setSavedIds(prev => new Set(prev).add(event.id))
-            // addSavedId({(
-                
-            //     (prev) => {new Set(prev).add(event.id)}
-            // )}
+            addSavedId(event.id);
+            // setSavedIds(prev => new Set(prev).add(event.id))
         } catch {
             alert('Failed to save event.')
         }
@@ -175,17 +182,28 @@ export default function SearchPage() {
                             </div>
                         ))}
                     </div>
-                    <ul className="w-full flex mt-auto justify-center">
-                        {results.totalPages > 0 && results.total > 10 && (
-                           <li
-                           className="mx-1 cursor-pointer p-2 text-gray-600 hover:text-gray-900"
-                                onClick={() => handlePageChange(keyword, city, page - 1)}
-                           >
-                                {`<`}
-                           </li>
-                        )}
 
-                        {windowStart > 1 && <li className="mx-1 p-2 text-gray-400">...</li>}
+                    <ul className="w-full flex justify-center items-center mt-auto text-sm">
+
+                        {windowStart > 1  && (
+                            <>
+                                <li
+                                    className="w-8 flex justify-center items-center mx-0.5 p-1 h-8 bg-blue-50 hover:bg-blue-100 text-gray-600 hover:text-gray-900 cursor-pointer text-sm"
+                                    onClick={() => handlePageChange(keyword, city, page - 1)}
+                                >
+                                    <svg viewBox="0 0 16 16" fill="none" className="size-6">
+                                        <path
+                                            d="M10.333 12.6667L5.66634 8.00004L10.333 3.33337"
+                                            stroke="currentColor"
+                                            strokeWidth="1"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        />
+                                    </svg>
+                                </li>
+                                <li className="mx-0.5 p-1 h-8 text-gray-400">...</li>
+                            </>
+                        )}
 
                         {results.totalPages > 0 && results.total > 10 && (
 
@@ -199,7 +217,7 @@ export default function SearchPage() {
                                     <li
                                         key={pageNumber}
                                         onClick={() => handlePageChange(keyword, city, pageNumber)}
-                                        className={`mx-1 cursor-pointer p-2 ${isActive ? 'font-bold text-blue-600 bg-blue-50' : 'text-gray-600'
+                                        className={`w-8 flex justify-center items-center mx-0.5 cursor-pointer p-1 h-8 bg-blue-50 hover:bg-blue-100 ${isActive ? 'font-bold text-blue-600 bg-blue-100' : 'text-gray-600'
                                     }`}
                                     >
                                         {pageNumber}
@@ -221,8 +239,8 @@ export default function SearchPage() {
                             //             key={i}
                             //             onClick={() => handlePageChange(keyword, city, pageNumber)}
                             //             className={`
-                            //             mx-1 cursor-pointer p-2
-                            //             ${isActive ? 'font-bold text-blue-600 bg-blue-50' : 'text-gray-600'}
+                            //             mx-1 cursor-pointer p-1
+                            //             ${isActive ? 'font-bold text-blue-600 h-8 bg-blue-50 hover:bg-blue-100' : 'text-gray-600'}
                             //             `}
                             //         >
                             //             {pageNumber}
@@ -231,16 +249,26 @@ export default function SearchPage() {
                             // })
                         )}
 
-                        {windowEnd < results.totalPages && <li className="mx-1 p-2 text-gray-400">...</li>}
-
-                        {results.totalPages > 10 && results.total > 10 && (
-                            <li
-                                className="mx-1 cursor-pointer p-2 text-gray-600 hover:text-gray-900"
-                                onClick={() => handlePageChange(keyword, city, page + 1)}
-                            >
-                                {`>`}
-                            </li>
+                        {windowEnd < results.totalPages && (
+                            <>
+                                <li className="mx-0.5 p-1 text-gray-400">...</li>
+                                <li
+                                    className="w-8 flex justify-center items-center mx-0.5 p-1 h-8 bg-blue-50 hover:bg-blue-100 text-gray-600 hover:text-gray-900 cursor-pointer text-sm"
+                                    onClick={() => handlePageChange(keyword, city, page + 1)}
+                                >
+                                    <svg viewBox="0 0 16 16" fill="none" className="size-6">
+                                        <path
+                                            d="M5.66699 12.6667L10.3337 8.00004L5.66699 3.33337"
+                                            stroke="currentColor"
+                                            strokeWidth="1"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        />
+                                    </svg>
+                                </li>
+                            </>
                         )}
+
                     </ul>
                 </div>
             )}
--- client/src/stores/searchStore.ts
+++ client/src/stores/searchStore.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import type { EventSearchResponse } from '../types/event'
+import { persist } from 'zustand/middleware'
 
 interface SearchStore {
     keyword: string
@@ -7,6 +8,7 @@ interface SearchStore {
     page: number
     results: EventSearchResponse | null
     savedIds: Set<string>
+
     setKeyword: (keyword: string) => void
     setCity: (city: string) => void
     setPage: (page: number) => void
@@ -14,22 +16,84 @@ interface SearchStore {
     addSavedId: (id: string) => void
 }
 
-export const useSearchStore = create<SearchStore>((set) => ({
-    keyword: '',
-    city: '',
-    page: 1,
-    results: null,
-    savedIds: new Set(),
-    setKeyword: (keyword) => set({ keyword }),
-    setCity: (city) => set({ city }),
-    setPage: (page) => set({ page }),
-    setResults: (results) => set({ results }),
-    addSavedId: (id) => set((state) => ({
-        savedIds: new Set(state.savedIds).add(id)
-    })),
-}))
+type PersistedState = {
+    keyword: string
+    city: string
+    page: number
+    results: EventSearchResponse | null
+    savedIds: string[]
+}
 
-// Study A
+export const useSearchStore = create<SearchStore>()(
+    persist(
+        (set) => ({
+            keyword: '',
+            city: '',
+            page: 1,
+            results: null,
+            savedIds: new Set(),
+
+            setKeyword: (keyword) => set({ keyword }),
+            setCity: (city) => set({ city }),
+            setPage: (page) => set({ page }),
+            setResults: (results) => set({ results }),
+
+            addSavedId: (id) => set((state) => ({
+                savedIds: new Set(state.savedIds).add(id)
+            })),
+        }),
+        {
+            name: 'search-store',
+
+            partialize: (state) => ({
+                keyword: state.keyword,
+                city: state.city,
+                page: state.page,
+                results: state.results,
+                savedIds: Array.from(state.savedIds),
+
+            }),
+
+            // merge: (persisted, current) => ({
+            //     ...current,
+            //     ...(persisted as Partial<SearchStore>),
+            //     savedIds: new Set((persisted as Any).savedIds as string[]),
+            // }),
+
+            merge: (persisted, current) => {
+                const p = persisted as PersistedState
+                return {
+                    ...current,
+                    keyword: p.keyword,
+                    city: p.city,
+                    page: p.page,
+                    results: p.results,
+                    savedIds: new Set(p.savedIds),
+                }
+            },    
+        }
+    )
+)
+
+
+
+// export const useSearchStore = create<SearchStore>((set) => ({
+//     keyword: '',
+//     city: '',
+//     page: 1,
+//     results: null,
+//     savedIds: new Set(),
+//     setKeyword: (keyword) => set({ keyword }),
+//     setCity: (city) => set({ city }),
+//     setPage: (page) => set({ page }),
+//     setResults: (results) => set({ results }),
+//     addSavedId: (id) => set((state) => ({
+//         savedIds: new Set(state.savedIds).add(id)
+//     })),
+// }))
+
+console.log('city --> ', useSearchStore)
+    // Study A
 // interface SearchStore {
 //     savedIds: Set<string>
 //     addSavedId: (id: string) => void
